a066fef06fac52ab9ede7c54c0286857cdad97ac
Bruno Bessa <beduardo2014@gmail.com>
2023-11-27 16:06:43 -0300

    Update date

--- backend/src/main/java/com/brunobessa/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/brunobessa/dscatalog/services/CategoryService.java
@@ -5,12 +5,15 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.brunobessa.dscatalog.dto.CategoryDTO;
 import com.brunobessa.dscatalog.entities.Category;
 import com.brunobessa.dscatalog.repositories.CategoryRepository;
+import com.brunobessa.dscatalog.services.exceptions.DatabaseException;
 import com.brunobessa.dscatalog.services.exceptions.ResourceNotFoundException;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -56,5 +59,18 @@ public class CategoryService {
 		catch(EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found" + id);
 		}
+	}
+
+	@Transactional(propagation = Propagation.SUPPORTS)
+	public void delete(Long id) {
+		if (!repository.existsById(id)) {
+			throw new ResourceNotFoundException("Recurso não encontrado");
+		}
+		try {
+	        	repository.deleteById(id);    		
+		}
+	    	catch (DataIntegrityViolationException e) {
+	        	throw new DatabaseException("Falha de integridade referencial");
+	   	}
 	}
 }
