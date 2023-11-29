127a94b1ffd7104078f2ac2b09e5c0b75b166549
Bruno Bessa <beduardo2014@gmail.com>
2023-11-29 10:24:55 -0300

    Product repository, service, resource, GET ok

---
+++ backend/src/main/java/com/brunobessa/dscatalog/dto/ProductDTO.java
@@ -0,0 +1,96 @@
+package com.brunobessa.dscatalog.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import com.brunobessa.dscatalog.entities.Category;
+import com.brunobessa.dscatalog.entities.Product;
+
+public class ProductDTO {
+	
+	
+	private Long id;
+	private String name;
+	private String description;
+	private Double price;
+	private String imgUrl;
+	
+	private List<CategoryDTO> categories = new ArrayList<>();
+	
+	public ProductDTO() {
+		
+	}
+
+	public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
+	}
+	
+	public ProductDTO(Product entity) {
+		this.id = entity.getId();
+		this.name = entity.getName();
+		this.description = entity.getDescription();
+		this.price = entity.getPrice();
+		this.imgUrl = entity.getImgUrl();
+	}
+	
+	public ProductDTO(Product entity, Set<Category> categories) {
+		this(entity);
+		categories.forEach(cat -> this.categories.add(new CategoryDTO(cat)));
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public List<CategoryDTO> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(List<CategoryDTO> categories) {
+		this.categories = categories;
+	}
+	
+	
+}
