0bbe4f5c79ac924113cdd23d7c0a8f7dac5848db
Bruno Bessa <beduardo2014@gmail.com>
2023-12-15 16:34:27 -0300

    starting tests

---
+++ backend/src/test/java/com/brunobessa/dscatalog/tests/Factory.java
@@ -0,0 +1,21 @@
+package com.brunobessa.dscatalog.tests;
+
+import java.time.Instant;
+
+import com.brunobessa.dscatalog.dto.ProductDTO;
+import com.brunobessa.dscatalog.entities.Category;
+import com.brunobessa.dscatalog.entities.Product;
+
+public class Factory {
+	
+	public static Product createProduct() {
+		Product product = new Product(1L, "Phone", "Good Phone", 800.0, "https://img.com/img.png", Instant.parse("2020-04-20T03:00:00Z"));
+		product.getCategories().add(new Category(2L, "Electronics"));
+		return product;
+	}
+	
+	public static ProductDTO createProductDTO() {
+		Product product = createProduct();
+		return new ProductDTO(product, product.getCategories());
+	}
+}
