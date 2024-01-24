cdcd6757c82ebe27b7490cf87c6547964a8f9535
Bruno Bessa <beduardo2014@gmail.com>
2024-01-24 09:16:01 -0300

    Custom validation response

--- backend/src/main/java/com/brunobessa/dscatalog/resources/exceptions/StandardError.java
+++ backend/src/main/java/com/brunobessa/dscatalog/resources/exceptions/StandardError.java
@@ -1,8 +1,10 @@
 package com.brunobessa.dscatalog.resources.exceptions;
 
+import java.io.Serializable;
 import java.time.Instant;
 
-public class StandardError {
+public class StandardError implements Serializable {
+	private static final long serialVersionUID = 1L;
 	
 	private Instant timestamp;
 	private Integer status;
