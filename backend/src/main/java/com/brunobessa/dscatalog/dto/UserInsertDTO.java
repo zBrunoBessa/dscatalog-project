2012ba4193e05efb6a7483477689f0c9da97417b
Bruno Bessa <beduardo2014@gmail.com>
2024-01-24 09:36:27 -0300

    Custom ConstraintValidator

--- backend/src/main/java/com/brunobessa/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/brunobessa/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.brunobessa.dscatalog.dto;
 
+import com.brunobessa.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 	private static final long serialVersionUID = 1L;
 	
