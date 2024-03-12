af4f4725c6bd769858ca5a88affeb97c95949b7a
Bruno Bessa <beduardo2014@gmail.com>
2024-03-12 08:39:02 -0300

    Routes

--- frontweb/src/components/ButtonIcon/index.tsx
+++ frontweb/src/components/ButtonIcon/index.tsx
@@ -1,10 +1,10 @@
-import './styles.css'
+import './styles.css';
 const ButtonIcon = () => {
-    return (
-        <button className="btn btn-primary btn-icon">
-            <h6>INICIA AGORA A SUA BUSCA</h6>
-        </button>
-    )
-}
+  return (
+    <button className="btn btn-primary btn-icon">
+      <h6>INICIA AGORA A SUA BUSCA</h6>
+    </button>
+  );
+};
 
-export default ButtonIcon
+export default ButtonIcon;
