65a99ef893508dc722311d25ade22979babb055c
Bruno Bessa <beduardo2014@gmail.com>
2024-03-22 09:42:37 -0300

    Props ProductCard, ProductPrice

--- frontweb/src/components/ProductCard/index.tsx
+++ frontweb/src/components/ProductCard/index.tsx
@@ -1,17 +1,22 @@
 import ProductPrice from 'components/ProductPrice';
 import './styles.css';
 
-import ProductImg from 'assets/images/product.png';
 
-const ProductCard = () => {
+import { Product } from 'types/product';
+
+type Props = {
+  product: Product;
+}
+
+const ProductCard = ( {product}: Props) => {
   return (
     <div className="base-card product-card">
       <div className="card-top-container">
-        <img src={ProductImg} alt="Nome do produto" />
+        <img src={product.imgUrl} alt={product.name} />
       </div>
       <div className="card-bottom-container">
-        <h6>Nome do produto</h6>
-        <ProductPrice />
+        <h6>{product.name}</h6>
+        <ProductPrice price={product.price}/>
       </div>
     </div>
   );
