f6f353cf4dd8d08a9ad09bfb9f28679bff97589d
Bruno Bessa <beduardo2014@gmail.com>
2024-03-22 09:06:38 -0300

    Props

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -18,7 +18,7 @@ const Home = () => {
           </div>
           <div>
             <Link to="/products">
-              <ButtonIcon />
+              <ButtonIcon text="Inicia agora a sua busca" />
             </Link>
           </div>
         </div>
