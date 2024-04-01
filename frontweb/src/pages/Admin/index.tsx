9228d18f6da5d5ab9e8a60b09256a871cea8e48c
Bruno Bessa <beduardo2014@gmail.com>
2024-04-01 08:45:51 -0300

    Admin Navbar layout mobile

--- frontweb/src/pages/Admin/index.tsx
+++ frontweb/src/pages/Admin/index.tsx
@@ -1,5 +1,16 @@
+import Navbar from './Navbar';
+
+import './styles.css';
+
 const Admin = () => {
-  return <h1>Pagina admin</h1>;
+  return (
+    <div className="admin-container">
+      <Navbar />
+      <div className="admin-content">
+        <h1>Conteúdo</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Admin;
