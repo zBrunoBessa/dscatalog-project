5845e659ca9203b15d0fe6697aba61361549e976
Bruno Bessa <beduardo2014@gmail.com>
2024-04-03 09:57:45 -0300

    Check user authenticated

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -1,6 +1,15 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import qs from 'qs'
 import history from './history';
+import jwtDecode from 'jwt-decode'
+
+type Role = 'ROLE_OPERATOR' | 'ROLA_ADMIN'
+
+type TokenData = {
+  exp: number;
+  user_name: string;
+  authorities: Role[];
+}
 
 
 type LoginResponse = {
@@ -81,3 +90,19 @@ axios.interceptors.response.use(function (response) {
 
   return Promise.reject(error);
 });
+
+export const getTokenData = () : TokenData | undefined => {
+
+  try {
+    return jwtDecode(getAuthData().access_token) as TokenData;
+  }
+  catch (error) {
+    return undefined
+  }
+}
+
+export const isAuthenticated = () : boolean => {
+  const tokenData = getTokenData();
+
+  return (tokenData && tokenData.exp * 1000 > Date.now()) ? true : false;
+}
