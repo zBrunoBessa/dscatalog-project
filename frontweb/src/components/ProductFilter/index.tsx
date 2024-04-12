09d2c03540e747a5a205454afbd39fb6f1ab21ad
Bruno Bessa <beduardo2014@gmail.com>
2024-04-12 08:43:18 -0300

    ProductFilter layout

--- frontweb/src/components/ProductFilter/index.tsx
+++ frontweb/src/components/ProductFilter/index.tsx
@@ -42,7 +42,7 @@ const ProductFilter = () => {
             placeholder="Nome do produto"
             name="name"
           />
-          <button>
+          <button className="product-filter-search-icon">
             <SearchIcon />
           </button>
         </div>
@@ -56,14 +56,17 @@ const ProductFilter = () => {
                   {...field}
                   options={selectCategories}
                   isClearable
-                  classNamePrefix="product-crud-select"
+                  placeholder="Categoria"
+                  classNamePrefix="product-filter-select"
                   getOptionLabel={(category: Category) => category.name}
                   getOptionValue={(category: Category) => String(category.id)}
                 />
               )}
             />
           </div>
-          <button className="btn btn-outline-secondary">LIMPAR</button>
+          <button className="btn btn-outline-secondary btn-product-filter-clear">
+            LIMPAR <span className="btn-product-filter-word">FILTRO </span>{' '}
+          </button>
         </div>
       </form>
     </div>
